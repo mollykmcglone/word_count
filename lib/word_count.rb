ad0af7c8ae8931fefe1bec63994175b3278fc696
Molly McGlone <mollykmcglone@gmail.com>
2016-07-01 09:49:30 -0700

    add code to create the core functionality of the method and pass the second and third specs

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,13 +1,13 @@
 require('pry')
 class String
   define_method(:word_count) do |check_word|
-    input_words = self.split[]
+    input_words = self.split
+    answer = 0
     input_words.each do |word|
       if word == check_word
       answer += 1
-      else
-      answer = 0
       end
     end
+    answer
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,12 +5,12 @@ describe('String#word_count') do
   it('will return 0 when the input word is not present in the input string of words') do
     expect(('the world is great').word_count('awesome')).to(eq(0))
   end
-  # it('will return 1 when the input word is present in the input string of words one time') do
-  #   expect(('the world is awesome').word_count('awesome')).to(eq(1))
-  # end
-  # it('will return the appropriate value when the input word is present in the input string of words two or more times') do
-  #   expect(('of all my acquaintances my dog is my favorite by far').word_count('awesome')).to(eq(3))
-  # end
+  it('will return 1 when the input word is present in the input string of words one time') do
+    expect(('the world is awesome').word_count('awesome')).to(eq(1))
+  end
+  it('will return the appropriate value when the input word is present in the input string of words two or more times') do
+    expect(('of all my acquaintances my dog is my favorite by far').word_count('my')).to(eq(3))
+  end
   # it('will handle uppercase user inputs') do
   #   expect(('WHAT THE WHAT IS GOING ON').word_count('WHAT')).to(eq(2))
   # end
