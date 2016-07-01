ccaf90a4a5bba7c1932078e12c0bae94e8c8a146
Molly McGlone <mollykmcglone@gmail.com>
2016-07-01 11:05:11 -0700

    add code to pass all remaining specs

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,10 +1,11 @@
 require('pry')
 class String
   define_method(:word_count) do |check_word|
-    input_words = self.split
+    input_words = self.downcase.gsub(/[^\w\s\d]/, '').split
+    check_word_new = check_word.downcase.gsub(/[^\w\s\d]/, '')
     answer = 0
     input_words.each do |word|
-      if word == check_word
+      if word == check_word_new
       answer += 1
       end
     end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -11,16 +11,16 @@ describe('String#word_count') do
   it('will return the appropriate value when the input word is present in the input string of words two or more times') do
     expect(('of all my acquaintances my dog is my favorite by far').word_count('my')).to(eq(3))
   end
-  # it('will handle uppercase user inputs') do
-  #   expect(('WHAT THE WHAT IS GOING ON').word_count('WHAT')).to(eq(2))
-  # end
-  # it('will handle mixed case user inputs') do
-  #   expect(('wHAt THE WhAT IS gOiNG oN').word_count('WHAT')).to(eq(2))
-  # end
-  # it('will handle user inputs with punctuation') do
-  #   expect(('What the what is going on??!').word_count('What')).to(eq(2))
-  # end
-  # it('will handle an input string longer than one sentence.') do
-  #   expect(('What the what is going on??! I cannot believe what is happening!').word_count('What')).to(eq(3))
-  # end
+  it('will handle uppercase user inputs') do
+    expect(('WHAT THE WHAT IS GOING ON').word_count('WHAT')).to(eq(2))
+  end
+  it('will handle mixed case user inputs') do
+    expect(('wHAt THE WhAT IS gOiNG oN').word_count('WHAT')).to(eq(2))
+  end
+  it('will handle user inputs with punctuation') do
+    expect(('What the what is going on??!').word_count('on')).to(eq(1))
+  end
+  it('will handle an input string longer than one sentence.') do
+    expect(('What the what is going on??! I cannot believe what is happening!').word_count('What')).to(eq(3))
+  end
 end
