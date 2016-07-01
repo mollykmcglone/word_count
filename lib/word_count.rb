861ceb9dc15340f8365ffcdb804a84ba2e8c1d1e
Molly McGlone <mollykmcglone@gmail.com>
2016-07-01 09:38:55 -0700

    add code to pass first spec

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,13 @@
 require('pry')
 class String
-  define_method(:word_count) do
-
-
+  define_method(:word_count) do |check_word|
+    input_words = self.split[]
+    input_words.each do |word|
+      if word == check_word
+      answer += 1
+      else
+      answer = 0
+      end
+    end
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,22 +5,22 @@ describe('String#word_count') do
   it('will return 0 when the input word is not present in the input string of words') do
     expect(('the world is great').word_count('awesome')).to(eq(0))
   end
-  it('will return 1 when the input word is present in the input string of words one time') do
-    expect(('the world is awesome').word_count('awesome')).to(eq(1))
-  end
-  it('will return the appropriate value when the input word is present in the input string of words two or more times') do
-    expect(('of all my acquaintances my dog is my favorite by far').word_count('awesome')).to(eq(3))
-  end
-  it('will handle uppercase user inputs') do
-    expect(('WHAT THE WHAT IS GOING ON').word_count('WHAT')).to(eq(2))
-  end
-  it('will handle mixed case user inputs') do
-    expect(('wHAt THE WhAT IS gOiNG oN').word_count('WHAT')).to(eq(2))
-  end
-  it('will handle user inputs with punctuation') do
-    expect(('What the what is going on??!').word_count('What')).to(eq(2))
-  end
-  it('will handle an input string longer than one sentence.') do
-    expect(('What the what is going on??! I cannot believe what is happening!').word_count('What')).to(eq(3))
-  end
+  # it('will return 1 when the input word is present in the input string of words one time') do
+  #   expect(('the world is awesome').word_count('awesome')).to(eq(1))
+  # end
+  # it('will return the appropriate value when the input word is present in the input string of words two or more times') do
+  #   expect(('of all my acquaintances my dog is my favorite by far').word_count('awesome')).to(eq(3))
+  # end
+  # it('will handle uppercase user inputs') do
+  #   expect(('WHAT THE WHAT IS GOING ON').word_count('WHAT')).to(eq(2))
+  # end
+  # it('will handle mixed case user inputs') do
+  #   expect(('wHAt THE WhAT IS gOiNG oN').word_count('WHAT')).to(eq(2))
+  # end
+  # it('will handle user inputs with punctuation') do
+  #   expect(('What the what is going on??!').word_count('What')).to(eq(2))
+  # end
+  # it('will handle an input string longer than one sentence.') do
+  #   expect(('What the what is going on??! I cannot believe what is happening!').word_count('What')).to(eq(3))
+  # end
 end
